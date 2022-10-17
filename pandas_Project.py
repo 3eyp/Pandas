418ba0e8adb353c917788dadbfddd7e6d7415f23
eyüp <class.py12@gmail.com>
2022-10-17 15:17:50 +0300

    Update pandas_Project.py

--- pandas_Project.py
+++ pandas_Project.py
@@ -3,12 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class Pandas():
-    
-    #  Pandas ile read_csv komutunu kullanarak .CSV uzantılı dosyadaki verileri çekme
-
     def __init__(self):
         self.read()
 
+    # Pandas ile read_csv komutunu kullanarak .CSV uzantılı dosyadaki verileri çekme
     def start_read(self):
         db = pd.read_csv("GBvideos.csv")
         return db
